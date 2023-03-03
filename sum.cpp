e774a06a3a217ae426f6ed729b1c28dfe5594e63
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2023-03-03 19:10:37 +0530

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-//Declaring the function sum
 int sum(int,int);
-
 int main(){
    int x, y;
    cout<<"enter first number: ";
@@ -14,22 +12,4 @@ int main(){
 
    cout<<"Sum of these two :"<<sum(x,y);
    return 0;
-}
-//Defining the function sum
-int get_nth_ancestor(const char *name, int len,
-			    unsigned char *result, int generation){
-	unsigned char sha1[20];
-	int ret = get_sha1_1(name, len, sha1);
-	if (ret)
-		return ret;
-
-	while (generation--) {
-		struct commit *commit = lookup_commit_reference(sha1);
-
-		if (!commit || parse_commit(commit) || !commit->parents)
-			return -1;
-		hashcpy(sha1, commit->parents->item->object.sha1);
-	}
-	hashcpy(result, sha1);
-	return 0;
 }
